5f78c418228e2bfeda9d4de3642b5f6055da147c
albertdm <albert-dm@outlook.com>
2019-08-08 00:13:58 -0300

    Service to search for movie

---
+++ src/services/movieService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { OMDB_API_KEY } from '../config';
+
+const API_URL = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}`;
+
+export const seekMovie = async ({ title, year }) => {
+  const { data, status } = await axios.get(`${API_URL}&s=${title}${year ? `&y=${year}` : ''}`);
+  if(status === 200){
+    if(data.Response === 'True'){
+      return data.Search;
+    } else{
+      throw new Error(data.Error);
+    }
+  } else {
+    throw new Error('Failed to fetch');
+  }
+
+};
