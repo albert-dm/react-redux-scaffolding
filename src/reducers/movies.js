8c43aae7b346397ff0e1877b0ab0c5bb87d25d14
albertdm <albert-dm@outlook.com>
2019-08-08 23:23:05 -0300

    Caching movies

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,7 +1,19 @@
-const movies = (state = [], action) => {
+let initialMovies;
+const savedMovies = JSON.parse(localStorage.getItem('movies'));
+
+if(savedMovies){
+  initialMovies = savedMovies;
+} else {
+  initialMovies = [];
+}
+
+const movies = (state = { list: initialMovies, selected: undefined }, action) => {
   switch (action.type) {
   case 'SET_MOVIES':
-    return [...action.movies];
+    localStorage.setItem('movies', JSON.stringify(action.movies));
+    return { ...state, list: [...action.movies] };
+  case 'SELECT_MOVIE':
+    return { ...state, selected: action.movie };
   default:
     return state;
   }
